9117876c330f502a34957a89a41ef0e706254313
marcinp38 <marcinp38@gmail.com>
2022-05-07 11:11:00 +0200

    Initial commit

---
+++ src/main/java/Porownanie.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Porownanie {
+    public static void main(String[] args) {
+        int a;
+        int b;
+        //main - public.static.void main
+        //sout - System.out.println
+
+        Scanner in = new Scanner(System.in);
+        System.out.print("Podaj a: ");
+        a = in.nextInt();
+        System.out.print("Podaj b: ");
+        b = in.nextInt();
+        if (a<b)
+            System.out.println("a mniejsze od b");
+        else if (a>b)
+            System.out.println("a wieksze od b");
+        else
+            System.out.println("a rowne b");
+
+
+    }
+}
+
+
