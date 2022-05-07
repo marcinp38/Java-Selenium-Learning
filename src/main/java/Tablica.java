dd6f92a175844a8eb672ac4196e35b2a48d3d01e
marcinp38 <marcinp38@gmail.com>
2022-05-07 15:30:48 +0200

    Tables exercises

---
+++ src/main/java/Tablica.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Tablica {
+    public static void main(String[] args) {
+
+        String table[] = new String[5];
+        Scanner in = new Scanner(System.in);
+
+        for (int i = 0; i < table.length; i++) {
+            System.out.print("give me " + (i + 1) + " name: ");
+            table[i] = in.nextLine();
+        }
+
+        int k=0;
+        while(k<5){
+            System.out.println(table[k]);
+            k++;
+        }
+
+    }
+}
---
+++ src/main/java/Tablica2.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Tablica2 {
+    public static void main(String[] args) {
+        String[] tablica = new String[5]; //indeksy 0-4
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Podaj rozmiar tablicy: ");
+        int n = in.nextInt();
+        int[] tab = new int[n];
+        for (int k = 0; k<n; k++) {
+            tab[k] = k+1;
+            System.out.println(tab[k]);
+        }
+    }
+}
