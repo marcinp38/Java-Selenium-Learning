bbbc9d49c1b177a496e4aa7f5d9c21f408176126
marcinp38 <marcinp38@gmail.com>
2022-05-08 11:14:12 +0200

    Exercise1&2-day2-Firma

--- src/main/java/Firma.java
+++ src/main/java/Firma.java
@@ -17,12 +17,28 @@ public class Firma {
         tester.nazwisko = "Nowak";
         tester.wiek = 26;
 
-        System.out.println(programistaMarcin.imie+ " " + programistaMarcin.nazwisko);
+        System.out.println(programistaMarcin.imie+" "+programistaMarcin.nazwisko+" lat: "+programistaMarcin.wiek);
         System.out.println(tester.wiek);
+
+        programistaMarcin.wyswietlDane();
+        tester.wyswietlDane();
     }
 }
 class Pracownik{
     String imie;
     String nazwisko;
     int wiek;
+
+    public void  wyswietlDane(){
+        System.out.println("Pracownik: "+imie+ " " +nazwisko+"\t"+wiek+"lat");
+    }
+    public String getImie(){
+        return imie;
+    }
+    public String getNazwisko(){
+        return nazwisko;
+    }
+    public int getWiek(){
+        return wiek;
+    }
 }
