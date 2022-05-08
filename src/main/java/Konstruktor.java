ee1a09fa8b48c94398691b23f5ac940d1c4493f2
marcinp38 <marcinp38@gmail.com>
2022-05-08 13:43:16 +0200

    Exercise1&2-day2-Konstruktory-toFinalize

---
+++ src/main/java/Konstruktor.java
@@ -0,0 +1,32 @@
+public class Konstruktor {
+    public static void main(String[] args) {
+
+        Samochod auto1 = new Samochod();
+        auto1.kolor = "czerwony";
+        auto1.marka = "Skoda";
+        auto1.rokProdukcji = 1984;
+
+        SamochodNowy auto = new SamochodNowy("czerwony", "skoda", 1984);
+        SamochodNowy auto2 = new SamochodNowy("bialy", "ford", 1700);
+    }
+}
+
+
+
+class Samochod {
+    int rokProdukcji;
+    String marka;
+    String kolor;
+}
+class SamochodNowy {
+    int rokProdukcji;
+    String marka;
+    String kolor;
+
+    public SamochodNowy(String kolor, String marka, int rokProdukcji) {
+        this.rokProdukcji = rokProdukcji;
+        this.kolor = kolor;
+        this.marka = marka;
+    }
+}
+
