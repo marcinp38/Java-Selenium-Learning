b74755f93a3fd226076aca5c505c5e74acf2fcda
marcinp38 <marcinp38@gmail.com>
2022-05-19 18:34:00 +0200

    Podcasts' functions added, next try for books failed

--- src/main/java/MyBooksAndPodcastsLibrary/Library.java
+++ src/main/java/MyBooksAndPodcastsLibrary/Library.java
@@ -11,17 +11,34 @@ public class Library {
         Books book3 = new Books("Dale", "Carnegie", "How to Win Friends and Influence People", Genre.Must_read);
         Books book4 = new Books("David", "Allen", "Getting Things Done",Genre.Self_development);
 
-        lib.add(book1);
-        lib.add(book2);
-        lib.add(book3);
-        lib.add(book4);
+            lib.add(book1);
+            lib.add(book2);
+            lib.add(book3);
+            lib.add(book4);
+
+
+        PodcastLibrary podLib = new PodcastLibrary();
+        Podcasts podcast1 = new Podcasts("The Evil Tester Show", "Alan Richardson",Genre.Must_listen);
+        Podcasts podcast2 = new Podcasts("Finanse Bardzo Osobiste","Marcin Iwuć",Genre.Should_listen);
+        Podcasts podcast3 = new Podcasts("Podcast Pasja Informatyki","Mirosław Zelent",Genre.Must_listen);
+        Podcasts podcast4 = new Podcasts("Podcast Stacja IT","Stacja IT",Genre.Should_listen);
+        Podcasts podcast5 = new Podcasts("Leśnodorski i Żurnalista w trójkącie towarzyskim","Żurnalista", Genre.Listen_for_relax);
+
+
+            podLib.add(podcast1);
+            podLib.add(podcast2);
+            podLib.add(podcast3);
+            podLib.add(podcast4);
+            podLib.add(podcast5);
 
 
         Scanner input = new Scanner(System.in);
         Boolean isExit = false;
         Boolean isBookExit = false;
+        Boolean isPodcastExit = false;
         String Command;
         String bookCommand;
+        String podcastCommand;
 
         welcomeNote();
         while (!isExit) {
@@ -32,6 +49,7 @@ public class Library {
                 case "b":{
                     lib.bookWelcomeNote();
                     while(!isBookExit) {
+                        System.out.println();
                         System.out.print("What do you want me to do? ");
                         bookCommand = input.nextLine();
                         switch (bookCommand) {
@@ -43,6 +61,7 @@ public class Library {
                                     System.out.println(books);
 
                                 }
+                                break;
                             case "s":{
                                 lib.provideAuthorData();
                                 System.out.println();
@@ -62,12 +81,45 @@ public class Library {
                         }
                     }
                 }
-                System.out.println();
+
                 break;
 
                 case "p": {
-                    System.out.println("You've picked podcasts. Please select an option:\n");
+                    podLib.podcastWelcomeNote();
+                    while(!isPodcastExit) {
+                        System.out.println();
+                        System.out.print("What do you want me to do? ");
+                        podcastCommand = input.nextLine();
+                        switch (podcastCommand) {
+                            case "r":
+                                podLib.showRandomPodcast();
+                                break;
+                            case "l":
+                                for (Podcasts podcast : podLib.podcastsList) {
+                                    System.out.println(podcast);
+
+                                }
+                                break;
+                            case "s":{
+//                                lib.provideAuthorData();
+//                                System.out.println();
+//                                System.out.println(lib.getAuthorsBooksList());
+////                                lib.showBooksByAuthor();
+
+
+                            }
+                            break;
+                            case "e":
+                                isPodcastExit = true;
+                                welcomeNote();
+                                break;
+                            default:
+                                System.out.println("Incorrect command.");
+                                break;
+                        }
+                    }
                 }
+                System.out.println();
                 break;
 
                 case "e":
@@ -91,4 +143,7 @@ public class Library {
 
 
     }
+
+
 }
+
